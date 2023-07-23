17a2657092416ee5fe790b05d34b62fdf28275c2
Leroy Scott Jr <leroysjr@yahoo.com>
2023-07-23 08:08:05 -0500

    Setting up card display

--- project/src/components/BookList.js
+++ project/src/components/BookList.js
@@ -4,6 +4,29 @@ const BookList = ({ books, removeSavedBook }) => {
     return (
         <div>
             <h2 className= "card text-center">Saved Books</h2>
+            <ul>
+                {books.map((book) => (
+                    <li key={book.id}>
+                        <div className='card text-center' style={{width: "18rem" }}>
+                            <div className='card-body'>
+                                <h5 className='title'>
+                                    <strong>Title: {book.title}</strong>
+                                </h5>
+                                <p className='card-text'>
+                                    Author: {book.author}
+                                </p>
+                                <p className='card-text'>
+                                    Reason To Check It Out: <br />
+                                </p>
+                                <p className='card-text'>
+                                    {book.description}
+                                </p>
+
+                            </div>
+
+                        </div>
+                    </li>
+            </ul>
         </div>
     )
 }
