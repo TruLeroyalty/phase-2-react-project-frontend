cddd99ade318343acc06a6058045e4d6f98f051e
Leroy Scott Jr <leroysjr@yahoo.com>
2023-07-23 07:47:48 -0500

    description useState

--- project/src/components/BookForm.js
+++ project/src/components/BookForm.js
@@ -3,3 +3,4 @@ import React, {useState} from 'react';
 const BookForm = ({onAddBook}) => {
     const [title, setTitle] = useState('');
     const [author, setAuthor] = useState('');
+    const [description, setDescription] = useState('');
