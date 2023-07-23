a6f0e24c1e7546c4125d17af16358f3d7883faa1
Leroy Scott Jr <leroysjr@yahoo.com>
2023-07-23 06:23:56 -0500

    Created a welcome on Home.js

---
+++ project/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    <div>
+        <h1>
+            <strong>
+                Welcome to your Favorite Library
+            </strong>
+        </h1>
+    </div>
+}
